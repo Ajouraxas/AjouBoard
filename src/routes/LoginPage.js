2dfd897835ff4d92e26640a3de1502e581998d9d
LUH <ung7497@ajou.ac.kr>
2022-02-25 15:18:19 +0900

    [AUTH] 이메일 로그인 구현

--- src/routes/LoginPage.js
+++ src/routes/LoginPage.js
@@ -1,7 +1,55 @@
-import React from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { authService } from "../lib/fbase";
 
 const LoginPage = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { value, name },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await signInWithEmailAndPassword(authService, email, password);
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <div>
+          <div />
+          <div>AJOURA 로그인</div>
+        </div>
+        <form onSubmit={onSubmit}>
+          <input name="email" type="email" onChange={onChange} value={email} />
+          <input
+            name="password"
+            type="password"
+            onChange={onChange}
+            value={password}
+          />
+          <button>로그인</button>
+        </form>
+        <button>Login with Google Account</button>
+        <form>
+          <span>이메일 찾기</span>
+          <span>회원 가입</span>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
