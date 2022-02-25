4bb93f1c3b0458eedcb339a0786aebb6dd91939d
LUH <ung7497@ajou.ac.kr>
2022-02-25 14:38:40 +0900

    [PAGE] Set Login page and Register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import ClubPage from './routes/ClubPage';
-import Home from './routes/Home';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import ClubPage from "./routes/ClubPage";
+import Home from "./routes/Home";
+import LoginPage from "./routes/LoginPage";
+import RegisterPage from "./routes/RegisterPage";
 
 /**
  * ClubPage 접속 : http://localhost:3000/#/club/123
@@ -11,8 +13,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path={'/'} element={<Home />}></Route>
-        <Route path={'/club/:clubId'} element={<ClubPage />}></Route>
+        <Route path={"/"} element={<Home />}></Route>
+        <Route path={"/login"} element={<LoginPage />}></Route>
+        <Route path={"/register"} element={<RegisterPage />}></Route>
+        <Route path={"/club/:clubId"} element={<ClubPage />}></Route>
       </Routes>
     </Router>
   );
