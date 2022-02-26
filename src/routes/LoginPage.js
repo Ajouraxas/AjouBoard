bc882458762701e5e156961f2f58b3354f3109bb
LUH <ung7497@ajou.ac.kr>
2022-02-26 16:36:43 +0900

    [CSS] Add css design on the login page.

--- src/routes/LoginPage.js
+++ src/routes/LoginPage.js
@@ -2,6 +2,8 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService } from "../lib/fbase";
+import "../style/reset.css";
+import styles from "../style/loginPage.module.css";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -22,7 +24,12 @@ const LoginPage = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
-      await signInWithEmailAndPassword(authService, email, password);
+      const user = await signInWithEmailAndPassword(
+        authService,
+        email,
+        password,
+      );
+      // console.log(user.user);
       navigate("/");
     } catch (error) {
       setEmail("");
@@ -36,26 +43,42 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <div>
-        <div>
-          <div />
-          <div>AJOURA 로그인</div>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.img} />
+          <div className={styles.title}>AJOURA 로그인</div>
         </div>
-        <form onSubmit={onSubmit}>
-          <input name="email" type="email" onChange={onChange} value={email} />
-          <input
-            name="password"
-            type="password"
-            onChange={onChange}
-            value={password}
-          />
-          <button>로그인</button>
+        <form onSubmit={onSubmit} className={styles.form}>
+          <label className={styles.label}>
+            <span className={styles.email}>이메일</span>
+            <input
+              name="email"
+              type="email"
+              onChange={onChange}
+              value={email}
+              className={styles.input}
+              placeholder="初めまして、私わみんぎゅです"
+            />
+          </label>
+          <label className={styles.label}>
+            <span className={styles.password}>파스워도</span>
+            <input
+              name="password"
+              type="password"
+              onChange={onChange}
+              value={password}
+              className={styles.input}
+            />
+          </label>
+          <button className={styles.button}>ログイン</button>
         </form>
-        <button>Login with Google Account</button>
+        <button className={styles.google}>Login with Google Account</button>
         <form>
-          <span>이메일 찾기</span>
-          <span onClick={onClick}>회원 가입</span>
+          <span className={styles.findEmail}>이메일 기억안나누 ㅋㅋ</span>
+          <span onClick={onClick} className={styles.register}>
+            회원가입
+          </span>
         </form>
       </div>
     </div>
