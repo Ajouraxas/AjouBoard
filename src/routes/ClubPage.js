d562321153e3840ef9ca2adc2d843a89b0bcfd65
LUH <ung7497@ajou.ac.kr>
2022-03-08 22:44:14 +0900

    [FIX] 즉시 실행 함수로 변경

--- src/routes/ClubPage.js
+++ src/routes/ClubPage.js
@@ -1,10 +1,11 @@
-import { doc, getDoc } from 'firebase/firestore';
-import { dbService } from '../lib/fbase';
-import React, { useEffect } from 'react';
-import PostNavbar from '../components/PostNavbar';
-import Posts from '../components/Posts';
-import { useNavigate, useParams } from 'react-router-dom';
-import style from '../style/ClubPage.module.css';
+import { doc, getDoc } from "firebase/firestore";
+import { dbService } from "../lib/fbase";
+import React, { useEffect } from "react";
+import PostNavbar from "../components/PostNavbar";
+import Posts from "../components/Posts";
+import { useNavigate, useParams } from "react-router-dom";
+import style from "../style/ClubPage.module.css";
+
 /**
  * component: PostNavber
  * useFor: 동아리 공지사항, 전체글, 개추 받은 글, 글쓰기
@@ -23,13 +24,12 @@ const ClubPage = ({ user }) => {
   const { clubId } = useParams();
   const navigate = useNavigate();
   useEffect(() => {
-    const isCorrectAccess = async () => {
-      const isCorrectAccess = await getDoc(doc(dbService, 'clubs', clubId));
+    (async () => {
+      const isCorrectAccess = await getDoc(doc(dbService, "clubs", clubId));
       if (isCorrectAccess.data() === undefined) {
-        return navigate('/clublist');
+        return navigate("/clublist");
       }
-    };
-    isCorrectAccess();
+    })();
   }, [clubId, navigate]);
 
   return (
