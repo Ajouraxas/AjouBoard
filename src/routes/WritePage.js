98eb674ac9a91fdc9e4e5e2a19ec713526e1e1dd
NSRBSG <nsrbsg.gmail.com>
2022-03-09 19:03:58 +0900

    [FIX] displayName에서 nickName 받는걸로 수정.

--- src/routes/WritePage.js
+++ src/routes/WritePage.js
@@ -1,12 +1,12 @@
-import { addDoc, collection, query } from "firebase/firestore";
-import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { dbService } from "../lib/fbase";
-import WriteBoard from "../components/WriteBoard";
-import style from "../style/WritePage.module.css";
-import { Helmet } from "react-helmet-async";
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { dbService } from '../lib/fbase';
+import WriteBoard from '../components/WriteBoard';
+import style from '../style/WritePage.module.css';
+import { Helmet } from 'react-helmet-async';
 
-const WritePage = ({ user: { displayName, uid } }) => {
+const WritePage = ({ user: { uid } }) => {
   const navigate = useNavigate();
   const {
     state: { clubId },
@@ -16,13 +16,16 @@ const WritePage = ({ user: { displayName, uid } }) => {
     const title = e.target[0].value;
     const content = e.target[1].value;
     const imageId = e.target[2].value;
+    const userData = await getDocs(
+      query(collection(dbService, 'users'), where('id', '==', uid))
+    );
     const q = query(collection(dbService, `clubs/${clubId}/posts`));
     await addDoc(q, {
       uid,
       content,
       createAt: Date.now(),
-      creatorName: displayName,
-      postType: "all",
+      creatorName: userData.docs[0].data().nickName,
+      postType: 'all',
       title,
       plusRecommendCount: 0,
       minusRecommendCount: 0,
